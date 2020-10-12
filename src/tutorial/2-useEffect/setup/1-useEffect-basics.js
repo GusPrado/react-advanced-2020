7d4d816f42d6fe3bd28b1b3ae0a04416433572d4
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-10-12 17:31:24 -0300

    useEffect & conditional rendering

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,32 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  // useEffect(() => {
+  //   console.log('useEffect called')
+  // }, [])
+  // console.log('component rendered')
+  const [value, setValue] = useState(0)
+
+  useEffect(() => {
+    // its the same of using second parameter -> []
+    // if (value >= 1) {
+
+    //   document.title = `New messages(${value})`
+    // }
+    if (value >= 1) {
+
+      document.title = `New messages(${value})`
+    }
+  }, [value]) // if blank just runs once at initial rendering
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Click me!
+      </button>
+    </>
+  )
 };
 
 export default UseEffectBasics;
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,27 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size,SetSize] = useState(window.innerWidth)
+
+  const checkSize = () => {
+    SetSize(window.innerWidth)
+  }
+ 
+  useEffect(() => {
+    console.log('useEffect')
+    window.addEventListener('resize', checkSize)
+    return () => {
+      console.log('cleanup')
+      window.removeEventListener('resize', checkSize)
+    }
+  })
+  console.log('render')
+  return (
+    <>
+      <h1>Window</h1>
+      <h2>{size} pixels</h2>
+    </>
+  )
 };
 
 export default UseEffectCleanup;
